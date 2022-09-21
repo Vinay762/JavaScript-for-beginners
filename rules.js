4b489e7c0214a6206e07436b6e5da76224eefeff
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 12:58:36 +0530

    Rules of Naming

---
+++ rules.js
@@ -0,0 +1,31 @@
+//rules for naming variables
+
+//you cannot start with numbers
+//1value(invalid);
+//value1 (valid);
+
+var value1 = 10;
+console.log(value1);
+console.log(value1 ** 5);
+
+//you can use only underscore _ or dollar symbol
+// first_name (valid);
+// _firstName (valid);
+
+//first$name(valid);
+// $firstName (valid);
+
+var $firstName = "Vinay";
+
+var first_name = "Tiwari" //snake case writing
+var firstName = "Priyanshu" //camel case writing
+
+console.log(firstName);
+
+
+// you cannot use spaces
+// first name (invalid);
+
+// convention 
+// start with small letters use use camelCase
+
