dad2144ee4c15e0388b3711826ccdf5e7c3e872e
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 19:12:28 +0530

    All loops in Array

---
+++ loopInArray.js
@@ -0,0 +1,58 @@
+//for loop in array
+
+// let fruits = ["apple", "mango", "grapes", "banana"];
+
+// let fruits2 = [];
+
+// for(let i = 0; i<fruits.length; i++){
+//     fruits2.push(fruits[i].toUpperCase());
+//     //console.log(fruits[i]);
+// }
+
+// console.log(fruits2);
+
+//Use Const for Creating Array
+
+
+// const fruits = ["apple", "mango"]; //address store main array heap 
+// //memory me stored hai ham kabhi bhi address ko change hi nahi karte
+
+// fruits.push("grapes");
+
+// //fruits = ["grapes", "pineapple"] ye error throw kar dega const ye 
+// //case me
+
+// console.log(fruits);
+
+
+//while loop in array
+   
+ //for of loop in array
+
+// let fruits = ["apple", "mango", "grapes"];
+
+// for(let fruit of fruits){
+//     console.log(fruit);
+// }
+
+// for in loop
+
+let fruits = ["apple", "mango", "grapes","banana"];
+
+for(let index in fruits){
+    console.log(fruits[index]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
