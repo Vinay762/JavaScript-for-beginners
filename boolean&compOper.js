2430b5072ed9c3b8c3d9c7e4a9e0f97a2f747b71
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 14:35:05 +0530

    Boolean & comparison operator

---
+++ boolean&compOper.js
@@ -0,0 +1,39 @@
+//boolean & comparison operator
+
+//booleans
+//  true or false;
+
+// let num1 = 5;
+// let num2 = 7;
+
+// console.log(num1 > num2);
+
+
+// // == vs ===
+
+// num1 = "7";
+// num2 = 7;
+
+// console.log(num1 == num2);
+
+// // == value check karege data type check nahi karega 
+
+// // === ye dono check karega
+
+// console.log(num1 === num2);
+
+
+// != vs !== 
+
+let num1 = 7;
+let num2 = 7;
+
+console.log(num1 != num2);
+
+
+num1 = "7";
+
+console.log(num1 != num2);
+
+console.log(num1 !== num2);
+
