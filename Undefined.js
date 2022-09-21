ffe402d38c867535f4f1c92df7f4b091438b4fac
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 14:14:53 +0530

    Undefined Null & BigInt

---
+++ Undefined.js
@@ -0,0 +1,44 @@
+//undefined
+
+// let firstName;
+// console.log(typeof(firstName));
+
+//jab varibles of bnakar usme koi value assign nahi karoge to 
+//uske type jo hai wo undefined hota hai
+
+//const ko turat assign karna padta hai just like c++;
+
+// firstName = "Vinay";
+// console.log(typeof(firstName), firstName);
+
+
+// //Null ki baat karta hai
+
+// let myVariable = null;
+// console.log(myVariable);
+// console.log(typeof(myVariable));
+// // null ka type object hota hai
+// // bug , errror hai iske resolve agar karne gaye to sari cheeze change
+// karni pad jayegi
+
+
+//BigInt
+
+// let myNumber = 124n;
+// console.log(myNumber);
+
+// console.log(Number.MAX_SAFE_INTEGER);
+
+// myNumber = BigInt(5455555555555545389346348);
+// console.log(myNumber);
+// console.log(typeof(myNumber));
+
+// //another way to do this 
+
+// let anotherNumber = 138n;
+// let secondOne = 1n;
+
+// console.log(secondOne + anotherNumber);
+
+//bigInt ko sirf bigInt ke sath joda ja sakta hai
+
