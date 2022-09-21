0feab40904da69fc41d2beaecbb61d17c9ba0686
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 13:05:47 +0530

    Let Keyword

---
+++ let.js
@@ -0,0 +1,13 @@
+//let keyword
+//declare variable with let keyword
+
+let firstName = "Vinay";
+console.log(firstName);
+
+//differnce b/w let & var
+
+//let firstName = "Tiwar" will throw error
+firstName = "Tiwar";
+console.log(firstName);
+
+//block scope vs function scope (covered later in this video)
