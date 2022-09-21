9e39db26308e8a405ed32b99b3f6ad85080a4c8e
Vinay <vinaytiwari1501@gmail.com>
2022-09-22 00:41:53 +0530

    Fuctions Declare & expression

---
+++ functions.js
@@ -0,0 +1,71 @@
+//functions in javascript 
+
+// function singHappyBirthday(){
+//     console.log("Happy Birthday to you ....");
+// }
+
+// //singHappyBirthday();
+
+// function twoplusfour(){
+//     return 2+4;
+// }
+
+// //call, run, invoke
+
+// console.log(twoplusfour());
+
+// function sum(a,b,c){
+//     return a+b+c;
+// }
+
+// console.log(sum(5,9));
+//console.log(2+3+undefined) -> returns nan not an number
+
+// odd or even
+// output : true if even
+
+// function even(a){
+//     return a%2 === 0;
+// }
+
+// console.log(even(5));
+
+//function string 
+// output firstCharacter
+
+// function firstChar(a){
+//     return a[0];
+// }
+
+// console.log(firstChar("Vinay"));
+
+//function
+//input : array, target(number);
+
+//output : index of target if target is present in array else -1;
+
+// function target(nums, target){
+//     for(let i = 0; i<nums.length; i++){
+//         if(nums[i] == target)return i;
+//     }
+//     return -1;
+// }
+
+// console.log(target([1,4,5,7,6], 9));
+
+
+//function expression
+
+// const singHappyBirthday = function(){
+//     console.log("Happy Birthday to you .... ");
+// }
+
+// singHappyBirthday();
+
+// const sumThreeNumbers = function(a,b,c){
+//     return a+b+c;
+// }
+
+// console.log(sumThreeNumbers(2,5,6));
+
+
