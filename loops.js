476846c9ea29a2944b4b84929ecdbcebf12df262
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 15:53:50 +0530

    for & while Loop

---
+++ loops.js
@@ -0,0 +1,21 @@
+// while loop
+
+// 0 to 9
+// let i = 0;
+// while(i<10){
+//     console.log(`${i}`);
+//     i = i+1;
+// }
+
+// for(let x = 0; x<10; x++){
+//     console.log(`${x}`);
+// }
+
+
+// let num = 10, sum = 0;
+// while(num > 0){
+//     sum += num;
+//     num--;
+// }
+// console.log(sum);
+
