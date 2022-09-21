d386bdafedb0403a939e840a09e4941ac3fda8e9
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 13:59:55 +0530

    More Knowledge on Strings

---
+++ data.js
@@ -0,0 +1,93 @@
+//typeof operator - data type batata hai
+
+//data types (primitive data types);
+//string "Vinay";
+//number 2,5,
+//booleans
+//undefined
+//null
+//BigInt
+//symbol
+
+
+// let age = 22;
+// let firstName = "Vinay";
+
+// console.log(typeof(age));
+// console.log(typeof(firstName));
+
+// //convert number to string
+
+// age = age + "";
+
+// console.log(typeof(age)); //"22";
+
+
+// //convert String to number
+
+// let myStr = +"34";
+// console.log(typeof(myStr));
+
+
+
+// let age = 18;
+// age = String(age); //another method;
+
+// console.log(typeof(age));
+
+// //string to number 
+
+// let mystr = "18";
+
+// mystr = Number(mystr);
+
+// console.log(typeof(mystr));
+
+
+// //string concatenation
+
+// let string1 = "Vinay";
+// let string2 =  "Tiwari";
+
+// let fullName = string1 + " " + string2;
+// console.log(fullName);
+
+
+// let string1 = "10";
+// let string2 = "18";
+
+// let newString = +string1 + +string2;
+// console.log(newString);
+
+// console.log(typeof(newString));
+
+
+//template string
+
+let age = 22;
+let firstName = "Vinay";
+
+// "my name is Vinay and my age is 22";
+// let about = "My name is " + firstName + " and my age is " + age;
+//console.log(about);
+
+let aboutMe = `my name is ${firstName} and my age is ${age}`;
+console.log(aboutMe);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
