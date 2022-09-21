0f83bd6fe4f767fc09db1282ee3e53923e4d1204
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 16:35:15 +0530

    Arrays  Intro

---
+++ arrays.js
@@ -0,0 +1,37 @@
+//intro to arrays
+
+//ordered collections of item
+
+// let fruits = [ "apple", "mango", "grapes"];
+// let numbers = [1,3,5,9];
+
+// let mixed = [1,3,5,"apple", null, undefined];
+
+
+// console.log(fruits);
+// console.log(numbers);
+
+// console.log(fruits[0]);
+
+// console.log(mixed);
+
+let fruits = [ "apple", "mango", "grapes"];
+// console.log(fruits);
+
+// fruits[1] = "banana";
+
+// console.log(fruits);
+
+// console.log(typeof(fruits));
+// //all the reference type data types are object
+
+// //reference type
+
+let obj = {}; //object literal
+
+console.log(Array.isArray(fruits));
+
+
+
+
+
