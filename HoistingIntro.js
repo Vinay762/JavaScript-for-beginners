904f4e1d7bf9918190a089f4c28d7f4a68225e65
Vinay <vinaytiwari1501@gmail.com>
2022-09-22 01:24:02 +0530

    Hoisting Intro

---
+++ HoistingIntro.js
@@ -0,0 +1,17 @@
+//hoisting 
+
+// hello(); //ye sirf function declaration k case me hoga befor intialisatio
+// // call kar diya 
+// // this behaviour is known as hoisting
+
+// function hello(){
+//     console.log("Hello World");
+// }
+
+console.log(hello); 
+
+//var k case me undefined baaki let & const k case me error aa jayega
+
+var hello = "hello world";
+console.log(hello);
+
