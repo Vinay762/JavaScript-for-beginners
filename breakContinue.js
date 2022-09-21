15eba2856f676222841e9fc9590bf338c7710470
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 16:20:48 +0530

    break Continue & Do While

---
+++ breakContinue.js
@@ -0,0 +1,26 @@
+//break keyword
+
+// for(let i = 1; i<=10; i++){
+//     if(i == 4){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// let sum = 0;
+
+// for(let x = 1; x <= 10; x++){
+//     if(x%2)continue;
+//     sum += x;
+// }
+// console.log(sum);
+
+
+//do while loop
+
+let i = 15;
+
+do{
+    console.log(i);
+    i++;
+}while(i<10);
