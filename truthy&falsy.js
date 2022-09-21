8a87400dc62eae47cf03fa0072472c4f37c0fa1b
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 14:52:11 +0530

    Truthy & falsy values

---
+++ truthy&falsy.js
@@ -0,0 +1,66 @@
+//truthy and falsy values
+
+
+
+// let age = 17;
+
+// if(age > 18){
+//     console.log("You can play game");
+// }else{
+//     console.log("You cannot play this game");
+// }
+
+
+// let num = 14;
+
+// if(num%2){
+//     console.log(`${num} is odd`);
+// }else{
+//     console.log(`${num} is even`);
+// }
+
+//falsy values
+// false
+// "";
+// null
+// undefined
+// 0
+
+//truty values 
+
+//false values k alawa baaki sab kuch truthy hai
+
+let firstName = "";
+
+if(firstName){
+    console.log(firstName);
+}else{
+    console.log("firstName is kinda empty");
+}
+
+//ternary opeator; also known as conditional operator
+//same as c++
+
+let age = 8;
+let drink = age > 18 ? "Daru" : "Milk";
+
+console.log(drink);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
