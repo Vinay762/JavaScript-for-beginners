4b9d71bd7b0ac4104a3f3df8fcc2c7900952746d
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 13:09:28 +0530

    Const Keyword

---
+++ const.js
@@ -0,0 +1,10 @@
+//declare constants
+
+const pi = 3.1428;
+
+// pi = 3.15;  assignment to const variable
+// const ki value ko change nahi kar sakte hai
+
+console.log(pi);
+
+console.log(pi ** 2);
