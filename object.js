0b7ca718788179e05dad64be5209519c00a5a6de
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 20:10:51 +0530

    Object Intro

---
+++ object.js
@@ -0,0 +1,54 @@
+//objects
+//arrays are good but not sufficient
+//for real world data
+//object store key value pairs
+//object don't have inddex;
+//object reference type hota hai
+
+// how to create object;
+
+const person = {
+    name: "Vinay",
+    age : 22,
+    hobbies : ["guitar", "sleeping", "listening", "music"]
+};
+
+console.log(person);
+
+//how to acess data from objects;
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.hobbies);
+
+//how to add key-value pair to an object;
+
+person.gender = "male";
+
+console.log(person);
+
+
+//console.log(person["name"]) returns Vinay;
+//console.log(person["age"]) return 22;
+//javascript object me default key ka data type string hota hai
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
