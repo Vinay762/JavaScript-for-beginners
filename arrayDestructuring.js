2c542b273cc1a0a5215aa0c0c5a3430dae72756b
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 19:57:50 +0530

    Array Destructuring

---
+++ arrayDestructuring.js
@@ -0,0 +1,47 @@
+// array destructuring 
+
+// const myArray = ["value1", "value2", "value3"];
+
+// // let myvar1 = myArray[0];
+// // let myvar2 = myArray[1];
+
+// // console.log("value of my var1", myvar1);
+// // console.log("value of my var2", myvar2);
+
+// let [myvar1, myvar2] = myArray;
+
+// console.log("value of my var1", myvar1);
+// console.log("value of my var2", myvar2);
+
+
+// //agar man le myArray me bas ek hi element hai to myvar2 undefined ho
+// //jayega
+
+// // let [myvar1, , myvar2] = ["value1", "value2", "value3"];
+
+const myArray = ["value1", "value2", "value3", "value4"];
+
+let [myvar1, myvar2] = myArray;
+
+let myNewArray = myArray.slice(2);
+
+// let [myvar1, myvar2, ...myNewArray] = myArray;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
