36dfed6c7fa18bba042900bf4b4d411bd3199ea9
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 20:48:32 +0530

    Computed Properties

---
+++ Commuted.js
@@ -0,0 +1,21 @@
+//computed Properties
+
+
+const key1 = "objkey1";
+const key2 = "objkey2";
+
+const value1 = "myvalue1";
+const value2 = "myvalue2";
+
+// const obj = {
+    
+// }
+
+const obj = {
+    [key1] : value1,
+    [key2] : value2,
+}
+
+console.log(obj);
+
+
