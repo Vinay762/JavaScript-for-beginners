db1875c09966ab2f1b45b04aaafe75a213d27e9e
Vinay <vinaytiwari1501@gmail.com>
2022-09-21 13:17:55 +0530

    String and its indexing

---
+++ stringIndexing.js
@@ -0,0 +1,15 @@
+// string indexing 
+
+let firstName = "Vinay";
+
+// same as C++;
+
+console.log(firstName[0]);
+
+//length of string
+
+console.log(firstName.length);
+
+//last index : length-1;
+
+console.log(firstName[firstName.length-1]);
