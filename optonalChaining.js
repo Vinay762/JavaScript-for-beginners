59111610e6646125b69fe0951aca33230a2a95c0
Vinay <vinaytiwari1501@gmail.com>
2022-09-23 01:48:37 +0530

    Optional Chaining

---
+++ optonalChaining.js
@@ -0,0 +1,9 @@
+//optional chaning
+
+const user = {
+    firstName : "Vinay",
+    //address : {houseNumber : '1234'},
+}
+
+console.log(user?.address?.houseNumber); //ye undefined dega 
+//error dene ki jagah
