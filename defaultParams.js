8584ba039c1eba434bc40a2fdaad1220810cc5d7
Vinay <vinaytiwari1501@gmail.com>
2022-09-22 19:32:28 +0530

    Default & Rest parameter

---
+++ defaultParams.js
@@ -0,0 +1,42 @@
+//default parameters
+
+// function addTwo(a,b){
+//     // if(typeof(b) === "undefined"){
+//     //     b = 0;
+//     // }
+//     return a+b;
+// }
+
+
+// function  addTwo(a,b=0){
+//     return a+b;
+// }
+
+
+// const ans = addTwo(4);
+// console.log(ans);
+
+//Rest Parameter
+
+// function myFunc(a,b,...c){
+//     console.log(`a is ${a}`);
+//     console.log(`b is ${b}`);
+//     console.log(`c is `, c);
+// }
+
+// myFunc(3,4,5,6,7,8,9);
+
+// function addAll(...numbers){
+//     let sum = 0;
+//     for(let number of numbers){
+//         sum += number;
+//     }
+//     return sum;
+// }
+
+// let sum = addAll(1,2,3,4,5,6,7,8,9);
+// console.log(sum);
+
+
+
+ 
