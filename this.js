f52454e2400dc32a26a5db40c0f15dd0efd57dad
Vinay <vinaytiwari1501@gmail.com>
2022-09-24 11:35:27 +0530

    this keyword

---
+++ this.js
@@ -0,0 +1,14 @@
+//console.log(this); //this likho ya window yahan par
+// function myFunc(){
+//     console.log(this);
+// }
+
+// window.myFunc();
+
+
+function myFunc(){
+    "use strict";
+    console.log(this);
+}
+
+myFunc();
