857765e5010f5f5df04393dcaaa79c302141abd3
Vinay <vinaytiwari1501@gmail.com>
2022-09-24 15:42:52 +0530

    Has Own Property

---
+++ hasOwnProperty.js
@@ -0,0 +1,14 @@
+//agar aap puri keys chahiye
+
+// for(let keys in user1){
+//     console.log(keys);
+// }
+
+//agar hame uske prototype wali keys nahi chahiye
+
+// for(let key in user1){
+//     if(user1.hasOwnProperty(key)){
+//         console.log(key);
+//     }
+// }
+
